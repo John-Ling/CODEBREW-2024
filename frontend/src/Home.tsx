197683b945cac5aff572b2a7cc7102679a2b8f3e
John <johnlingbusiness@gmail.com>
2024-04-05 12:28:59 +0800

    Tasks now persist after closing app

--- frontend/src/Home.tsx
+++ frontend/src/Home.tsx
@@ -1,7 +1,31 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import SearchBox from "./components/SearchBox";
 import "./assets/css/home.css";
 
 const Home = () => {
+    const navigate = useNavigate();
+    if (localStorage.getItem("tasks") === null) {
+        localStorage.setItem("tasks", JSON.stringify([]));
+    }
+
+    let tasks: string | null = localStorage.getItem("tasks");
+    if (tasks === null) {
+        return <>
+        </>
+    }
+
+    useEffect(() => {
+        if (tasks === null) {
+            return;
+        }
+        if (JSON.parse(tasks).length != 0) {
+            navigate("/schedule");
+        }
+        return;
+    })
+
+    console.log(tasks);
     return (
         <main id="main-view">
             <h1>What's Happening Today?</h1>
